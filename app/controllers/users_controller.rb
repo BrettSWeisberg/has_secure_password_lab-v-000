31c6c5737b2892441b4d03b9fdd4339f066d4625
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-11 21:10:38 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
